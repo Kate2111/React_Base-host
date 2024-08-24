86f188e1871f1ce0e5aa0c630506ea225a32cbb0
Kate2111 <noskova2111@mail.ru>
2024-08-24 18:43:09 +0300

    Настройка webpack-dev-server и переменных окружения env

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,23 +1,27 @@
 import webpack from 'webpack';
 import {buildWebpackConfig} from "./config/buildConfig/buildWebpackConfig";
-import {BuildOptions, BuildPath} from "./config/buildConfig/types/config";
+import {BuildEnv, BuildOptions, BuildPath} from "./config/buildConfig/types/config";
 import path from "path";
 
-const paths: BuildPath = {
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    build: path.resolve(__dirname, 'build'),
-    html: path.resolve(__dirname, 'public', 'index.html')
-}
+export default (env: BuildEnv) => {
+    const paths: BuildPath = {
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        build: path.resolve(__dirname, 'build'),
+        html: path.resolve(__dirname, 'public', 'index.html')
+    }
 
-const mode = 'development'
-const isDev = mode === 'development';
+    const mode = env.mode || 'development'
+    const PORT = env.port || 3000
 
-const options: BuildOptions = {
-    mode: 'development',
-    paths,
-    isDev
-}
+    const isDev = mode === 'development';
 
-const config: webpack.Configuration = buildWebpackConfig(options);
+    const config: webpack.Configuration = buildWebpackConfig({
+            mode,
+            paths,
+            isDev,
+            port: PORT,
+        }
+    )
 
-export default config;
+    return config
+};
