fddf2c13133f338d256e0634dd081e019e3bbf5e
Kate2111 <noskova2111@mail.ru>
2024-08-24 17:22:14 +0300

    Базовые настройки webpack

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
-export function someFn(arg: string) :string {
-    console.log(arg)
+export function someFn(arg: number): string {
+    console.log('RANDOM FUNCTION')
 
-    return '';
+    return ''
 }
