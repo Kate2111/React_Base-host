750f6d95541750b73445958b0a434412dfcd9d0f
Kate2111 <noskova2111@mail.ru>
2024-08-25 15:49:19 +0300

    Настройка css modules

--- config/buildConfig/buildLoaders.ts
+++ config/buildConfig/buildLoaders.ts
@@ -1,26 +1,35 @@
 import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
 
-export function buildLoaders() : webpack.RuleSetRule[] {
+export function buildLoaders({isDev}: BuildOptions) : webpack.RuleSetRule[] {
     const typeScriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
     };
 
-    const scssLoader = {
+    const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
-            // Creates `style` nodes from JS strings
-            "style-loader",
-            // Translates CSS into CommonJS
-            "css-loader",
-            // Compiles Sass to CSS
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev
+                            ? "[path][name]__[local]"
+                            : '[hash:base64:8]',
+                    },
+                }
+            },
             "sass-loader",
         ],
     }
 
     return [
         typeScriptLoader,
-        scssLoader
+        cssLoader
     ]
 }
