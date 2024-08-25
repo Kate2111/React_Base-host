e16a11e253d60660608ac75101449eea29b04b06
Kate2111 <noskova2111@mail.ru>
2024-08-25 16:37:43 +0300

    Настройка route и lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import {Counter} from "./components/Counter";
 import './index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import {lazy, Suspense} from "react";
+
+
+const MainPageLazy = lazy(() => import('./pages/MainPage/MainPage'));
+//@ts-ignore
+const AboutPageLazy = lazy(() => new Promise(resolve => setTimeout(() => resolve(import('./pages/AboutPage/AboutPage')), 1500)));
 
 const App = () => {
     return (
         <div className="app">
-            Counter
-            <Counter/>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="/" element={<MainPageLazy/>} />
+                    <Route path="/about" element={<AboutPageLazy/>} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import {Counter} from "../../components/Counter";
+
+const MainPage = () => {
+    return (
+        <div>
+            <Counter/>
+        </div>
+    );
+};
+
+export default MainPage;
