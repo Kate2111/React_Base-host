ee55f1611c7d57b3e0be10fe45c5bfb8af372c40
Kate2111 <noskova2111@mail.ru>
2024-08-26 19:57:59 +0300

    Добавлена светлая и темная тема

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import './index.scss'
+import './styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
 import {lazy, Suspense} from "react";
-
+import {useTheme} from "./theme/useTheme";
 
 const MainPageLazy = lazy(() => import('./pages/MainPage/MainPage'));
 //@ts-ignore
 const AboutPageLazy = lazy(() => new Promise(resolve => setTimeout(() => resolve(import('./pages/AboutPage/AboutPage')), 1500)));
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle Theme</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
